a0e0602be78dbe5e27004badffdaff9023bfde4d
sotul04 <109208105+sotul04@users.noreply.github.com>
2024-05-16 10:03:47 +0700

    test: add test setter for good class entity

--- tests/good/test_good.py
+++ tests/good/test_good.py
@@ -17,3 +17,16 @@ class TestGood:
         assert unit_good.get_price == 132000.0
         assert unit_good.get_stock == 10
         assert unit_good.get_imgSource == ""
+    
+    def test_create_good_sets(self):
+        unit_good = Good()
+        unit_good.set_idItem = 10
+        unit_good.set_name = "Jagung"
+        unit_good.set_price = 50000.0
+        unit_good.set_stock = 10
+        unit_good.set_imgSource = "ini_gambar_dummy"
+        assert unit_good.get_idItem == 10
+        assert unit_good.get_name == "Jagung"
+        assert unit_good.get_price == 50000.0
+        assert unit_good.get_stock == 10
+        assert unit_good.get_imgSource == "ini_gambar_dummy"
